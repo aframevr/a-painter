943e437a667cce0228c31db2d46485627a88cc12
Fernando Serrano <fernandojsg@gmail.com>
2016-09-14 18:35:45 +0200

    Added tick to spheres

--- src/brushes/spheres.js
+++ src/brushes/spheres.js
@@ -14,12 +14,20 @@ var spheres = {
     var sphere = new THREE.Mesh(this.geometry, this.material);
     var sca = this.data.size / 2  * pressure;
     sphere.scale.set(sca, sca, sca);
+    sphere.initialScale = new THREE.Vector3(sca, sca, sca);
     sphere.position.copy(pointerPosition);
     sphere.rotation.copy(rotation);
 
     this.object3D.add(sphere);
-
+    console.log(this.object3D);
     return true;
+  },
+  tick: function (time, delta) {
+    for (var i = 0; i < this.object3D.children.length; i++ ) {
+      var sphere = this.object3D.children[i];
+      var sin = (Math.sin(i + time/500.0) + 1) / 2 + 0.1;
+      sphere.scale.copy(sphere.initialScale).multiplyScalar(sin);
+    }
   }
 };
 
