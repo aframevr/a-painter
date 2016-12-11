ebbe33b3dd85f67ab5877598889d3ce9e6696dfa
machenmusik <machenmusik@gmail.com>
2016-12-11 11:17:51 -0500

    auto-enter VR; also, HMD auto-enter(exit) on vrdisplay(de)activate

---
+++ src/components/scene/auto-enter-vr.js
@@ -0,0 +1,31 @@
+/* global AFRAME */
+var utils = AFRAME.utils;
+
+/**
+ * Automatically enter VR, either upon vrdisplayactivate or immediately if possible.
+ */
+AFRAME.registerComponent('auto-enter-vr', {
+  schema: {default: true},
+
+  init: function () {
+    var scene = this.el;
+
+    if (utils.getUrlParameter('auto-enter-vr') === 'false') { return; }
+
+    window.addEventListener('vrdisplayactivate', function () {
+      scene.enterVR();
+    }, false);
+
+    window.addEventListener('vrdisplaydeactivate', function () {
+      scene.exitVR();
+    }, false);
+
+    // just try to enter... turns out we need to wait for next tick
+    setTimeout(function () { scene.enterVR(); }, 0);
+  },
+
+  update: function () {
+    return (!this.data) ? this.el.exitVR() : this.el.enterVR();
+  }
+});
+
--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ require('./components/paint-controls.js');
 require('./components/ui.js');
 require('./components/ui-raycaster.js');
 
+require('./components/scene/auto-enter-vr.js');
+
 require('./brushes/line.js');
 require('./brushes/stamp.js');
 require('./brushes/spheres.js');
