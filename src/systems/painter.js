21c00ba8f96426f636b58729a81c0a736757ebfd
Diego F. Goberna <diego@feiss.be>
2016-12-15 11:20:19 +0100

    removed author in banner

--- src/systems/painter.js
+++ src/systems/painter.js
@@ -28,7 +28,7 @@ AFRAME.registerSystem('painter', {
       document.getElementById('logo').setAttribute('visible', false);
       document.getElementById('acamera').setAttribute('orbit-controls', 'position', '0 1.6 3');
       document.getElementById('apainter-logo').classList.remove('hidden');
-      document.getElementById('apainter-author').classList.remove('hidden');
+      //document.getElementById('apainter-author').classList.remove('hidden'); // not used yet
     }
 
     this.startPainting = false;
