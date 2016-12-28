4bcc09fcb793d7361a69d221a6b0efb55f5581a2
Michael Feldstein <msfeldstein@gmail.com>
2016-12-27 16:13:59 -0800

    Snow Brush: Make all flakes start at cursor position, dont calculate random in the vertex shader, cap the minimum flake size

--- src/brushes/snow.js
+++ src/brushes/snow.js
@@ -2,30 +2,30 @@
 (function(){
   var FLAKES_PER_POINT = 2;
   var vertexShader = `
-    attribute float linePosition;
+    attribute float randValue;
+    attribute float birthTime;
 
     uniform float time;
     uniform float size;
 
     varying float alpha;
 
-    float rand(vec2 co){
-      return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
-    }
-
-    float rand(float i) {
-      return rand(vec2(i));
-    }
-
     void main() {
-      gl_PointSize = max(size * 100., 2.0);
-      float r = rand(linePosition);
-      float life = 1.0 - fract(time / (30.0 + 20.0 * r) + 120.0 * r);
+      gl_PointSize = max(size * 100., 5.0);
+      float r = randValue;
+      float t = time - birthTime;
+      // Give each flake its own random period between 30 and 50, and have
+      // it cycle from 1.0 to 0.0 repeatedly.
+      float life = 1.0 - fract(t / (30.0 + 20.0 * r));
+      // X and Z will oscillate in a sinusoidal pattern of period 10,
+      // while Y simply falls based on its life's speed.
       float xoff = cos(time / 10.0 + r * 10.0) / 30.0;
-      float yoff = life / 2.0 + sin(time / 10.0 + r * 10.0) / 30.0 - 0.25;
+      float yoff =( 1.0 - life) / -2.0;
       float zoff = sin(time / 10.0 + r * 120.0) / 30.0;
       vec3 pos = position + vec3(xoff, yoff, zoff);
       gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
+      // The flake will fade in when it is born, and fade out before dying,
+      // only to fade back in from (near) it's origin.
       alpha = 1.0 - (life - 0.5) * (life - 0.5) * 4.0;
     }
   `;
@@ -49,12 +49,14 @@
         this.idx = 0;
         this.geometry = new THREE.BufferGeometry();
         this.vertices = new Float32Array(this.options.maxPoints * 3 * FLAKES_PER_POINT);
-        this.linePositions = new Float32Array(this.options.maxPoints * FLAKES_PER_POINT);
+        this.randValues = new Float32Array(this.options.maxPoints * FLAKES_PER_POINT);
+        this.birthTime = new Float32Array(this.options.maxPoints * FLAKES_PER_POINT);
         this.currentLinePosition = 0;
 
         this.geometry.setDrawRange(0, 0);
         this.geometry.addAttribute('position', new THREE.BufferAttribute(this.vertices, 3).setDynamic(true));
-        this.geometry.addAttribute('linePosition', new THREE.BufferAttribute(this.linePositions, 1).setDynamic(true));
+        this.geometry.addAttribute('randValue', new THREE.BufferAttribute(this.randValues, 1).setDynamic(true));
+        this.geometry.addAttribute('birthTime', new THREE.BufferAttribute(this.randValues, 1).setDynamic(true));
 
         this.material = new THREE.ShaderMaterial({
           vertexShader: vertexShader,
@@ -83,14 +85,16 @@
       addPoint: function (position, orientation, pointerPosition, pressure, timestamp) {
         for (var i = 0; i < FLAKES_PER_POINT; i++) {
           var linepos = this.currentLinePosition++
-          this.linePositions[linepos] = linepos;
+          this.randValues[linepos] = Math.random();
+          this.birthTime[linepos] = Date.now() - this.startTime;
           this.vertices[ this.idx++ ] = pointerPosition.x;
           this.vertices[ this.idx++ ] = pointerPosition.y;
           this.vertices[ this.idx++ ] = pointerPosition.z;
         }
 
         this.geometry.attributes.position.needsUpdate = true;
-        this.geometry.attributes.linePosition.needsUpdate = true;
+        this.geometry.attributes.randValue.needsUpdate = true;
+        this.geometry.attributes.birthTime.needsUpdate = true;
 
         this.geometry.setDrawRange(0, this.data.numPoints * FLAKES_PER_POINT);
 
