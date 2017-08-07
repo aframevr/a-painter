e96bc4e89aa98d5fca6d20c9672ab12f6be4fe8e
haydenjameslee <haydenjameslee@gmail.com>
2017-08-07 11:29:32 +0700

    Removes old comments from sync.js

--- src/systems/sync.js
+++ src/systems/sync.js
@@ -15,12 +15,10 @@ AFRAME.registerSystem('sync', {
 
     this.el.addEventListener('stroke-started', function (evt) {
       var json = evt.detail.stroke.getJSON(brushSystem);
-      // console.log('sending new stroke', json);
       NAF.connection.broadcastDataGuaranteed('stroke-started', json);
     });
 
     this.el.addEventListener('stroke-point-added', function(evt) {
-      // console.log('sending point', evt.detail);
       delete evt.detail.target;
       NAF.connection.broadcastDataGuaranteed('stroke-point-added', evt.detail);
     });
@@ -28,14 +26,12 @@ AFRAME.registerSystem('sync', {
     /* Receiving */
 
     NAF.connection.subscribeToDataChannel('stroke-started', function (senderId, type, data, targetId) {
-      // brushSystem.loadJSON({version: 1, strokes: [stroke], brushes: brushSystem.brushes});
       var brush = data.brush;
       var color = new THREE.Color().fromArray(brush.color);
       brushSystem.addStroke(brush.id, brush.name, color, brush.size)
     });
 
     NAF.connection.subscribeToDataChannel('stroke-point-added', function (senderId, type, data, targetId) {
-      // console.log('receiving point', data);
       data.position = toVector3(data.position);
       data.pointerPosition = toVector3(data.pointerPosition);
       data.orientation = toQuat(data.orientation);
