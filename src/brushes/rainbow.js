014550ca744f8a40bf0162b19fd57ba8c6685c30
Fernando Serrano <fernandojsg@gmail.com>
2017-08-31 01:05:39 +0200

    Bump dist

--- src/brushes/rainbow.js
+++ src/brushes/rainbow.js
@@ -1,31 +1,27 @@
 /* globals AFRAME THREE */
 (function(){
-  var vertexShader = `
-    varying vec2 vUv;
-    void main() {
-      vUv = uv;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-    }
-  `;
+  var vertexShader = "varying vec2 vUv; \
+    void main() { \
+      vUv = uv; \
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 ); \
+    }";
 
-  var fragmentShader = `
-    uniform sampler2D tDiffuse;
-    uniform float amount;
-    uniform float time;
-    varying vec2 vUv;
-
-    vec3 hsv2rgb(vec3 c) {
-        vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-        vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-        return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-    }
-
-    void main() {
-      float h = mod(vUv.x - time / 3000.0, 1.0);
-      vec4 color = vec4(hsv2rgb(vec3(h, 1.0, 0.5)), 1.0);
-      gl_FragColor = color;
-    }
-  `;
+  var fragmentShader = "uniform sampler2D tDiffuse; \
+    uniform float amount; \
+    uniform float time; \
+    varying vec2 vUv; \
+    \
+    vec3 hsv2rgb(vec3 c) { \
+        vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0); \
+        vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www); \
+        return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y); \
+    } \
+    \
+    void main() { \
+      float h = mod(vUv.x - time / 3000.0, 1.0); \
+      vec4 color = vec4(hsv2rgb(vec3(h, 1.0, 0.5)), 1.0); \
+      gl_FragColor = color; \
+    }";
 
   var material = new THREE.ShaderMaterial({
     vertexShader: vertexShader,
