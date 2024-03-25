d34b90307608cbb765a2f62787a36b90ed5fe775
Antonio Pisano <antonio@elettrotecnica.it>
2024-03-25 21:39:36 +0100

    Replace THREE.XHRLoader with THREE.FileLoader

--- src/systems/brush.js
+++ src/systems/brush.js
@@ -493,7 +493,7 @@ AFRAME.registerSystem('brush', {
     console.timeEnd('Binary Loading');
   },
   loadFromUrl: function (url, binary) {
-    var loader = new THREE.XHRLoader(this.manager);
+    var loader = new THREE.FileLoader(this.manager);
     loader.crossOrigin = 'anonymous';
     if (binary === true) {
       loader.setResponseType('arraybuffer');
